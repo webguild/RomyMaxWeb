0fee7c63ed5bb206751e077223d79a925c9bdae7
Prokopenko Alex <a.prokopenko@web-guild.net>
2013-06-05 17:22:34 +0300

    Загрузка на гит

---
+++ scripts/main.js
@@ -0,0 +1,154 @@
+﻿$(function(){
+	$(".header li.active").prev().find("a").css("background", "none");
+});
+
+$(function(){
+	$(".serie .tabs li").live('click',function(){
+		if(!$(this).hasClass('active')){
+			$(this).addClass('active').siblings('.active').removeClass('active');
+			$(this).parent().siblings('.content').find('.tab-inner.active').removeClass('active').siblings('.tab-inner').addClass('active');
+		}
+	});
+});
+$(function(){
+    $(".header a.compareLink").click(
+      function () {
+         $(this).addClass('compareHover');
+		 $('body').animate({ 'scrollTop':0});
+		 $('#modal-compare').modal('show');
+		 return false;
+		 
+      }
+    );
+	
+	$('#modal-compare').on('hide', function () {
+		$('a.compareLink').removeClass('compareHover');
+	})
+	$('.question').click (function(){
+        $(this).parent().find('.tech-conflict').fadeToggle();
+    });
+});
+
+
+/*$(function(){
+	$('.logo-block li').live('click',function(){
+		var that=$(this),
+		parent = that.parent(),
+		sib = parent.siblings('.techs'),
+		num = parent.find('li').index(that),
+		active = sib.find('.tech-item.active'),
+		newActive = sib.find('.tech-item').eq(num),
+		hidden = active.find('.hidden-block');
+		
+		//sib.find('.tech-item').eq(num).addClass('active').siblings('.active').removeClass('active');
+		hidden.slideUp(function(){
+			hidden.removeClass('active');
+			active.fadeOut(function(){
+				$(this).removeClass('active');
+				newActive.fadeIn(function(){
+					$(this).addClass('active')
+					
+				});
+			});
+		});
+		
+		$('body').animate({ 'scrollTop':sib.offset().top-70 });
+		
+		
+		
+		
+		
+		
+		
+		that.addClass('active').siblings('.active').removeClass('active');
+	});
+	
+	$('.tech-item .close-block').live('click',function(){
+		$(this).closest('.tech-item').find('.hidden-block').slideUp(function(){
+			$(this).removeClass('active');
+		});
+		
+		$('body').animate({ 'scrollTop':$(this).closest('.techs').offset().top-70 });
+		return false;
+	});
+	
+	
+	$('.tech-item .show').live('click',function(){
+		$(this).closest('.tech-item').find('.hidden-block').slideDown(function(){
+			$(this).addClass('active');
+		});
+		$(this).text('Свернуть').removeClass('show').addClass('hide');
+		return false;
+	});
+	
+	$('.tech-item .hide').live('click',function(){
+		$(this).closest('.tech-item').find('.hidden-block').slideUp(function(){
+			$(this).removeClass('active');
+		});
+		$(this).text('Узнать больше').removeClass('hide').addClass('show');
+		return false;
+	});
+});*/
+$(function () {
+    $('.slider')
+        .anythingSlider({
+            resizeContents: true,
+            addWmodeToObject: 'opaque'
+        })
+});
+$(function () {
+    $('#videoSlider')
+        .anythingSlider({
+            resizeContents: true,
+            addWmodeToObject: 'opaque'
+        })
+        // Initialize video extension
+        // see https://developers.google.com/youtube/player_parameters?hl=en#Parameters for a list of parameters
+        .anythingSliderVideo({
+            // video id prefix; suffix from $.fn.anythingSliderVideo.videoIndex
+            videoId: 'asvideo',
+            // auto load YouTube api script
+            youtubeAutoLoad: true,
+            // see: https://developers.google.com/youtube/player_parameters#Parameters
+            youtubeParams: {
+                modestbranding: 1,
+                iv_load_policy: 3,
+                fs: 1
+            }
+        });
+});
+
+
+//Запуск слайдера с прокруткой
+$(function () {
+
+	var sliders = $('.techs').scrollSlider({ fadeSpeed: 500 });
+	sliders = sliders.add( $('.seria-slider-wrap').scrollSlider({
+		slideSelector: '.seria-slider li',
+		paginator: '.seria-slider-pag',
+		prevSelector: '.left-arrow',
+		nextSelector: '.right-arrow'
+	}) );
+
+        /*
+		var sliders = $('.techs').scrollSlider(options);
+		
+		//Настройки по-умолчанию
+		ScrollSLider.defaults = {
+			paginator: 'next', || можно записать в виде селектора
+			slideSelector: '.tech-item',
+			openButtonSelector: 'a.show',
+			closeButtonSelector: 'div.close-block',
+			hidenBlockSelector: '.hidden-block',
+			prevSelector: '.prev',
+			nextSelector: '.next',
+			offsetTop: -40,
+			offsetBottom: -40,
+			fadeSpeed: 300
+		};
+        */
+});
+
+
+
+
---
+++ scripts/scroll.slider.wg.js
@@ -0,0 +1,226 @@
+﻿	
+;(function (){
+
+	
+
+	//Обертка в стиле jQuery
+	$.prototype.scrollSlider = function (options) {
+		
+		return $(this).each(function () {
+			return new ScrollSLider(this, options);
+		});
+	};
+
+	//Конструктор плагина
+	function ScrollSLider (element, options) {
+		//Настройки базовых отношений
+		this.el = element;
+		element.ui = this;
+		this.$el = $(this.el);
+		this.options = $.extend(ScrollSLider.defaults, options);
+
+		//Инициализация
+		this.initialize();
+	}
+
+	//Настройки плагина по-умолчанию
+	ScrollSLider.defaults = {
+		paginator: 'next',
+		slideSelector: '.tech-item',
+		openButtonSelector: 'a.show',
+		closeButtonSelector: 'div.close-block, .close',
+		hidenBlockSelector: '.hidden-block',
+		prevSelector: '.prev',
+		nextSelector: '.next',
+		offsetTop: -40,
+		offsetBottom: -40,
+		fadeSpeed: 500,
+		trottleTime: 200
+	};
+
+	
+	//Инициализатор
+	ScrollSLider.prototype.initialize = function () {
+		this.$slides = this.$el.find(this.options.slideSelector);
+
+		(new WindowListener()).listenTo(this);
+		
+		this.inRange = false;
+		this.isSlideOpen = false;
+		this.isTrottle = false;
+		
+		if (ScrollSLider.defaults.paginator == 'next')
+			this.$paginator = this.$el.next();
+		else 
+			this.$paginator =  this.$el.find(this.options.paginator);
+
+		this.$pages = this.$paginator.children();
+
+		this.activeIdx = 0;
+		this.setActive();
+		
+		this.$pages.on('click', {self: this}, this.pagerClick);
+		this.$el.on('click', this.options.openButtonSelector, {self: this}, this.openSlide);
+		this.$el.on('click', this.options.closeButtonSelector, {self: this}, this.closeSlide);
+		this.$el.on('click', this.options.prevSelector, {self: this}, this.prevNext);
+		this.$el.on('click', this.options.nextSelector, {self: this}, this.prevNext);
+		
+		
+		
+	};
+
+	//Смена слайда
+	ScrollSLider.prototype.setActive = function () {
+		//Смена указателя на пейджере
+		this.$pages.eq(this.activeIdx).addClass('active')
+			.siblings().removeClass('active');
+
+		//Смена слайда с анимацией
+		this.$slides.eq(this.activeIdx).fadeIn(this.options.fadeSpeed)
+			.siblings().stop(true, true).fadeOut(0);
+	}
+
+	//Функции обработки событий
+	//Открытие слайда
+	ScrollSLider.prototype.openSlide = function (e) {
+		e.preventDefault();
+
+		var $this = $(e.currentTarget);
+		var self = e.data.self;
+		
+		$this.fadeOut(self.options.fadeSpeed).closest(self.options.slideSelector)
+			.find(self.options.hidenBlockSelector).fadeIn(self.options.fadeSpeed);
+
+		self.isSlideOpen = true;
+		
+	}
+
+	//Закрытие слайда
+	ScrollSLider.prototype.closeSlide = function (e) {
+		e.preventDefault();
+
+		var $this = $(e.currentTarget);
+		var self = e.data.self;
+
+		$this.closest(self.options.slideSelector)
+			.find(self.options.hidenBlockSelector).fadeOut(self.options.fadeSpeed)
+			.end().find(self.options.openButtonSelector).fadeIn(self.options.fadeSpeed);
+
+		//$(document).scrollTop(self.$el.offset().top - 100);
+		$('body,html').animate({ 'scrollTop': self.$el.offset().top - 100 });
+
+		self.isSlideOpen = false;
+
+	}
+
+	
+	ScrollSLider.prototype.pagerClick = function (e) {
+		e.preventDefault();
+
+		var $this = $(e.currentTarget);
+		var self = e.data.self;
+
+		self.activeIdx = $this.index();
+		self.setActive();
+	};
+
+	ScrollSLider.prototype.prevNext = function (e) {
+		e.preventDefault();
+
+		var $this = $(e.currentTarget);
+		var self = e.data.self;
+		
+		
+		self.activeIdx += $this.is(self.options.prevSelector) ? -1 : 1; 
+		self.activeIdx = self.activeIdx < 0 ? 0 : self.activeIdx;
+		self.activeIdx = self.activeIdx >= self.$pages.length ? self.$pages.length - 1 : self.activeIdx;
+		
+		self.setActive();
+	};
+
+	ScrollSLider.prototype.scrollCallback = function (e, delta, scrollSpeed) {
+		//delta: -1 - вниз, +1 - вверх
+		if (this.isTrottle) {
+			e.preventDefault();
+			return;
+		}
+		
+		this.isTrottle = true;
+		var self = this;
+		setTimeout(function () {
+			self.isTrottle = false;
+		}, this.options.trottleTime);
+		
+		if (delta < 0 && this.activeIdx < this.$pages.length - 1 && !this.isSlideOpen) {
+			e.preventDefault();
+			this.activeIdx++;
+			this.setActive();
+		}
+
+		if (delta > 0 && this.activeIdx > 0 && !this.isSlideOpen) {
+			e.preventDefault();
+			this.activeIdx--;
+			this.setActive();
+		}
+		
+	};
+
+
+	//Window Singleton listener ****************************************************************************
+	function WindowListener () {
+		if (WindowListener.singelton)
+			return WindowListener.singelton;
+
+		
+		this.listeners = [];
+		$(window).on('mousewheel', {self: this}, this.windowMousewheel);
+		$(window).on('scroll', {self: this}, this.windowScroll);
+		
+		WindowListener.singelton = this;
+
+	}
+
+	//Add Listener 
+	WindowListener.prototype.listenTo = function (obj) {
+		this.listeners.push(obj);
+	};
+
+	//Window Event Callbacks
+	//Mousewheel Event Callback
+	WindowListener.prototype.windowMousewheel = function (e, delta, deltaX, deltaY) {
+		var self = e.data.self;
+		for (var i = 0; i < self.listeners.length; i++) {
+			var obj = self.listeners[i];
+			if (obj.inRange) {
+				obj.scrollCallback(e, deltaY, WindowListener.scrollSpeed || 0 );
+			}
+				
+		}
+
+	};
+
+	//Windowscroll Event Callback
+	WindowListener.prototype.windowScroll = function (e) {
+		var self = e.data.self;
+		
+		
+		for (var i = 0; i < self.listeners.length; i++) {
+			var obj =  self.listeners[i];
+//console.log($(document).scrollTop() < obj.$el.offset().top + obj.options.offsetTop, $(document).scrollTop(), obj.$el.offset().top + obj.options.offsetTop)
+			//Определение попадания слайдера в пределы окна
+			if ( $(document).scrollTop() < obj.$el.offset().top + obj.options.offsetTop
+				&& ( $(document).scrollTop() + $(window).height() ) > 
+					(obj.$el.offset().top + obj.$el.outerHeight(true) + obj.$paginator.outerHeight(true) + obj.options.offsetBottom) 
+			) {
+				obj.inRange = true;
+			} else {
+				obj.inRange = false;
+			}
+
+		}
+		
+	};
+
+
+	
+})();
